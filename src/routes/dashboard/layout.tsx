2b6e09541a6f545bfac2bb531b5bdb84eaea905a
Usman Butt <usman.butt@outlook.com>
2024-08-14 15:31:40 -0400

    push to remote

---
+++ src/routes/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import { component$, Slot } from "@builder.io/qwik";
+import { SideNav } from "~/components/ui/dashboard/sidenav";
+
+export default component$(() => {
+  return (
+    <div class="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div class="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div class="flex-grow md:overflow-y-auto md:p-12">
+        <Slot />
+      </div>
+    </div>
+  );
+});
