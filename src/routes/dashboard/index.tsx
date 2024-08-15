92bd1489cd1fbe6c3641fd8302a768caf7007e83
Usman Butt <usman.butt@outlook.com>
2024-08-15 15:03:03 -0400

    implement streaming with useResource$() and <Resource /> to implement Loading, not supported till v2 within routeLoader$()

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,28 +1,30 @@
-import { Resource, component$ } from "@builder.io/qwik";
+import { Resource, component$, useResource$ } from "@builder.io/qwik";
 import { RevenueChart } from "~/components/ui/dashboard/revenue-chart";
 import { Card } from "~/components/ui/dashboard/cards";
 import { LatestInvoices } from "~/components/ui/dashboard/latest-invoices";
-import { routeLoader$ } from "@builder.io/qwik-city";
 import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "~/lib/data";
 
-export const useFetchData = routeLoader$(() => {
-  return async () => {
+export default component$(() => {
+  const dataResource = useResource$(async ({ cleanup }) => {
+    // A good practice is to use `AbortController` to abort the fetching of data if
+    // new request comes in. We create a new `AbortController` and register a `cleanup`
+    // function which is called when this function re-runs.
+    const controller = new AbortController();
+    cleanup(() => controller.abort());
+
     const [revenue, latestInvoices, cardData] = await Promise.all([
       fetchRevenue(),
       fetchLatestInvoices(),
       fetchCardData(),
     ]);
     return { revenue, latestInvoices, cardData };
-  };
-});
+  });
 
-export default component$(() => {
-  const data = useFetchData();
   return (
     <main>
       <h1 class="lusitana mb-4 text-xl md:text-2xl">Dashboard</h1>
       <Resource
-        value={data}
+        value={dataResource}
         onResolved={({ revenue, latestInvoices, cardData }) => {
           const {
             totalPaidInvoices,
